f1de537ea801593c95704870bd29c24dde9c0178
bd-ans <elaytpatters@gmail.com>
2022-12-07 14:24:58 +0500

    zsh:1: command not found: git

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-// import HeaderSec from './components/Header-sec'; /* IMPORT HEADER SECTION */
+import HeaderSec from './components/Header-sec'; /* IMPORT HEADER SECTION */
 import FooterSec from './components/Footer-sec'; /* IMPORT FOOTER SECTION */
-// import VideoSec from './components/Video-sec'; /* IMPORT VIDEO SECTION */
+import VideoSec from './components/Video-sec'; /* IMPORT VIDEO SECTION */
 function App() { /* EXPORT APP */
   return (
     <>
       {/* HEADER SECTION */}
-      {/* <HeaderSec /> */}
+      <HeaderSec />
       {/* VIDEO SECTION */}
-      {/* <VideoSec /> */}
+      <VideoSec />
       {/* FOOTER SECTION */}
       <FooterSec />
     </>
