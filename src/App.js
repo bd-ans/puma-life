a2bc0d75e0f45830c00097e338dde0e9900b1b6b
bd-ans <elaytpatters@gmail.com>
2022-12-07 14:20:23 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import HeaderSec from './components/Header-sec'; /* IMPORT HEADER SECTION */
+// import HeaderSec from './components/Header-sec'; /* IMPORT HEADER SECTION */
 import FooterSec from './components/Footer-sec'; /* IMPORT FOOTER SECTION */
 // import VideoSec from './components/Video-sec'; /* IMPORT VIDEO SECTION */
 function App() { /* EXPORT APP */
   return (
     <>
       {/* HEADER SECTION */}
-      <HeaderSec />
+      {/* <HeaderSec /> */}
       {/* VIDEO SECTION */}
       {/* <VideoSec /> */}
       {/* FOOTER SECTION */}
