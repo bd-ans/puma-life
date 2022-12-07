05ec7c62c950c834fd8f648fa3295e1bb287c0ee
bd-ans <elaytpatters@gmail.com>
2022-12-07 12:40:46 +0500

    changes made to the video section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import HeaderSec from './components/Header-sec'; /* IMPORT HEADER SECTION */
 import FooterSec from './components/Footer-sec'; /* IMPORT FOOTER SECTION */
+import VideoSec from './components/Video-sec'; /* IMPORT VIDEO SECTION */
 function App() { /* EXPORT APP */
   return (
     <>
       {/* HEADER SECTION */}
       <HeaderSec />
+      {/* VIDEO SECTION */}
+      <VideoSec />
       {/* FOOTER SECTION */}
       <FooterSec />
     </>
---
+++ src/components/Video-sec.js
@@ -0,0 +1,31 @@
+export default function VideoSec() {
+  return (
+    <>
+      {/* VIDEO SECTION */}
+      <div id="video-section" className="video-section">
+        <div className="container">
+          <div className="video-inner">
+            {/* VIDEO LEFT CONTENT */}
+            <div className="video-left-content">
+              <div className="video-left-top-content">
+                <h2 className="video-left-top-content-txt">VIDEO</h2>
+              </div>
+              <div className="video-left-center-content">
+                <h1 className="video-left-center-content-txt">Desire the feel of <br /> satisfaction.</h1>
+              </div>
+              <div className="video-left-bottom-content">
+                <p className="video-left-bottom-content-txt">Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br /> Neque auctor proin amet sagittis semper ipsum at <br /> lectus. At </p>
+              </div>
+            </div>
+            {/* VIDEO RIGHT CONTENT */}
+            <div className="video-right-content">
+              <div className="video-right-top-content">
+                ewfw
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
